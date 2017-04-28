635cd3bd18a4c1a876b9d5c58bc63b4b2e604c11
Tim <Tim.mouse@gmail.com>
2017-04-29 06:40:41 +0800

    router demo Step 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,14 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+      <h1>Angular Router</h1>
+    <nav>
+      <a routerLink="/crisis-center" routerLinkActive="active">Crisis Center</a>
+      <a routerLink="/heroes" routerLinkActive="active">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
